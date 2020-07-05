2978ba67099a51cedf25530350ebcf391e0ff780
Dennis Clark <drclark1985@gmail.com>
2020-07-04 18:40:48 -0600

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,63 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return "Delicious " + name;
+
+  //nameMenuItem("Pizza");
+  //nameMenuItem("Burrito");
+  //nameMenuItem("Taco")
+};
+
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name,
+    price,
+    type,
+  };
+  return menuItem;
+}
+
+function addIngredients(ingredient, ingredients) {
+  if (ingredients.includes(ingredient)) return;
+  ingredients.push(ingredient);
+};
+
+
+function formatPrice(price) {
+  return "$" + price;
+};
+
+
+function formatPrice(initialPrice) {
+  return "$" + initialPrice;
+}
+//price is $6.00 so sum of 6.00 * .90 would result in answer
+function decreasePrice(menuItem) {
+  return menuItem * .90;
+}
+
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title,
+    ingredients,
+    type,
+
+
+  }
+  return recipe;
 
 }
+
+
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
